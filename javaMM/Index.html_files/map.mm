f8a5c08aaa0e62768569a3f55d26d234d887fd05
avatarUNO <49586862+avatarUNO@users.noreply.github.com>
2020-04-13 10:43:22 +0200

    aa
    aa

---
+++ javaMM/Index.html_files/map.mm
@@ -0,0 +1,89 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="hola" FOLDED="false" ID="ID_1433626497" CREATED="1585852568067" MODIFIED="1585852575099" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.948" layout="OUTLINE">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="26" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="a" FOLDED="true" POSITION="right" ID="ID_1097101081" CREATED="1585852808351" MODIFIED="1585852809778">
+<edge COLOR="#0000ff"/>
+<node TEXT="solo una emoci&#xf3;n.." ID="ID_23585524" CREATED="1585852809781" MODIFIED="1585852998738"/>
+<node TEXT="mira, somos dos.." ID="ID_711920692" CREATED="1585852998991" MODIFIED="1585853005066"/>
+<node TEXT="eso es el perd&#xf3;n.." ID="ID_73213926" CREATED="1585853008576" MODIFIED="1585853013987"/>
+<node TEXT="" ID="ID_651658537" CREATED="1585853015504" MODIFIED="1585853015504"/>
+</node>
+<node TEXT="b" FOLDED="true" POSITION="right" ID="ID_597291918" CREATED="1585853614519" MODIFIED="1585853619930">
+<edge COLOR="#00ff00"/>
+<node TEXT="unos nos desintegramos..&#xa;otros se benefician de ello" ID="ID_1083727495" CREATED="1585853621007" MODIFIED="1585853633073"/>
+<node TEXT="la vida como una maldita vela&#xa;No te compares" ID="ID_980740736" CREATED="1585853633311" MODIFIED="1585853682163"/>
+<node TEXT="necesito estar a buenas&#xa;con alguien mas" ID="ID_375584599" CREATED="1585853682336" MODIFIED="1585853697746"/>
+<node TEXT="y el resto me d&#xe1; igual" ID="ID_737111327" CREATED="1585853697983" MODIFIED="1585853718441"/>
+<node TEXT="contemplas la vida?&#xa;Te supera, nos supera" ID="ID_613017801" CREATED="1585853747735" MODIFIED="1585853762867"/>
+<node TEXT="y sin embargo, estamos en ella&#xa;formamos parte de &#xe9;sta&#xa;sientate en tu silla, y espera&#xa;porque no hay, no hay salida" ID="ID_611433822" CREATED="1585853770847" MODIFIED="1585853832350"/>
+<node TEXT="en el grito, nos sentimos unidos a la naturaleza&#xa;grita, no hay l&#xed;mite, grita!!, grita m&#xe1;s fuerte!!&#xa;Qui&#xe9;n te escucha?&#xa;agudiza tu vista, mira.." ID="ID_1718409871" CREATED="1585853882190" MODIFIED="1585853960852"/>
+</node>
+<node TEXT="c" FOLDED="true" POSITION="right" ID="ID_1994784860" CREATED="1585854174478" MODIFIED="1585854175025">
+<edge COLOR="#ff00ff"/>
+<node TEXT="y" ID="ID_20726339" CREATED="1585854177951" MODIFIED="1585854312576"/>
+</node>
+</node>
+</map>
